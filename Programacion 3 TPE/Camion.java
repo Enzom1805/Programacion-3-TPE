228595dfd64065e0b2f7461b5a6d13e7d21ca0ac
annaknell <annitaknell@gmail.com>
2026-06-14 20:14:21 -0300

    .

--- Programacion 3 TPE/Camion.java
+++ Programacion 3 TPE/Camion.java
@@ -1,16 +1,18 @@
+import java.util.ArrayList;
 
 public class Camion {
     private String id_camion;
     private String patente;
     private boolean esta_refrigerado;
     private int capacidad_kg;
+    private ArrayList<Paquete> paquetesCargados;
     
     public Camion(String id_camion,String patente, int capacidad_kg, boolean esta_refrigerado){
         this.id_camion = id_camion;
         this.patente = patente;
         this.capacidad_kg = capacidad_kg;
         this.esta_refrigerado = esta_refrigerado;
-        
+        this.paquetesCargados = new ArrayList<>();
     }
 
     //getters
@@ -18,6 +20,9 @@ public class Camion {
     public String getPatente() {return patente;}
     public int getCapacidad_kg() {return capacidad_kg;}
     public boolean esta_refrigerado(){return esta_refrigerado;}
+    public ArrayList<Paquete> getPaquetesCargados(){
+        return new ArrayList<>(paquetesCargados);
+    }
 
     //setters
     public void setId_camion(String id_camion) {this.id_camion = id_camion;}
--- Programacion 3 TPE/Servicios.java
+++ Programacion 3 TPE/Servicios.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 public class Servicios {
+    private Solucion mejorSolucionBack;
+    private int mejorPesoNoAsignado;
+    private int estadosGenerados;
     private ArrayList<Camion> camiones;
     private ArrayList<Paquete> paquetes;
     private HashMap<String, Paquete> mapaPaquete;
-    public Servicios() {
-        this.camiones = new ArrayList<>();
-        this.paquetes = new ArrayList<>();
+    public Servicios(ArrayList<Camion> camiones, ArrayList<Paquete> paquetes) {
+        this.camiones = camiones;
+        this.paquetes = paquetes;
         
         this.mapaPaquete = new HashMap<>();
         for(Paquete p : this.paquetes){
@@ -24,10 +27,10 @@ public class Servicios {
         for(Paquete p : this.paquetes) {
             if(p.contiene_alimentos() == buscaAlimento) {
                 filtrados.add(p);
+            }
         }
+        return filtrados;  
     }
-    return filtrados;  
-}
 
     public ArrayList<Paquete> servicio3(int minimaUrgencia, int maximaUrgencia) {
         ArrayList<Paquete> filtrados = new ArrayList<>();
@@ -37,5 +40,32 @@ public class Servicios {
             }
         }
         return filtrados;
-    }   
+    }
+     /*Se desea establecer una asignación de todos los paquetes a los camiones
+    disponibles con el objetivo de minimizar el peso total de los paquetes que no
+    pudieron ser asignados a ningún camión.
+    Se sabe que existen ciertas restricciones para asignar un paquete a un camión:
+    •
+    Primero, ningún camión podrá superar su capacidad máxima de carga. La
+    capacidad de cada camión está definida en el archivo de entrada.
+    •
+    Segundo, los paquetes que contienen alimentos sólo podrán ser asignados
+    a camiones refrigerados*/
+    public Solucion asignarPaquetesBack(){
+        this.me
+        back(asignados, mejorAsignacion, 0, 0, 0);
+    }
+    private void back(ArrayList<Paquete> asignados, ArrayList<Paquete> mejorAsignacion, int pesoActual, int mejorPeso, int index){
+
+        if(index == paquetes.size()){
+            if(pesoActual > mejorPeso){
+                mejorPeso = pesoActual;
+            }
+        }
+
+
+
+
+        for(int i = 0; i )
+    }
 }
